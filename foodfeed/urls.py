d6b88e874a949d892c9a5f17b886e614cabe9c26
Iqrorjon Islomov <111178357+IqrorjonCoder@users.noreply.github.com>
2022-11-18 09:33:18 +0500

    donee

--- foodfeed/urls.py
+++ foodfeed/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.main, name="main page"),
     path('main/', views.main, name="main page"),
+    path('card', views.card, name="card page"),
     path('<animal_name>/', views.animal_name),
     path('<animal_name>/<animal_item>/', views.animal_item),
+    path('<animal_name>/<animal_item>/<item_type>/', views.food_type),
+    path('<animal_name>/<animal_item>/<item_type>/<food_name>', views.food),
 ]
--- foodfeed/views.py
+++ foodfeed/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def main(request):
     return render(request, 'main.html')
+
+def card(request):
+    return render(request, "card.html")
 
 
 def animal_name(request, animal_name):
@@ -12,3 +16,16 @@ def animal_name(request, animal_name):
 def animal_item(request, animal_name, animal_item):
     return render(request, "animal_items.html", context={"name": animal_name,
                                                          "animal_items": animal_item})
+
+
+def food_type(request, animal_name, animal_item, item_type):
+    return render(request, "animal_type.html", context={"name": animal_name,
+                                                         "animal_items": animal_item,
+                                                         "item_type": item_type})
+
+
+def food(request, animal_name, animal_item, item_type, food_name):
+    return render(request, "food.html", context={"name": animal_name,
+                                                         "animal_items": animal_item,
+                                                         "item_type": item_type,
+                                                        "food_name": food_name})
